9c38ad8812692d4936fa57ee1e4c71ea5fb426f3
RishawnG <32467633+RishawnG@users.noreply.github.com>
2017-10-19 20:47:54 -0500

    Initial commit

---
+++ maze.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("boundary1").addEventListener("mouseover", turnRed);
+document.getElementById("boundary1").addEventListener("mouseout", mouseOut);
+
+function turnRed(){
+	boundary1.setattribute(div.youlose())
+}
+
+
+
+
+
+
