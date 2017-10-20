85c5c2eba9cc6498995fa4feb11cf34dc58a757b
RishawnG <32467633+RishawnG@users.noreply.github.com>
2017-10-20 15:27:46 -0500

    Ex 2
    Completed exercise 2

--- maze.js
+++ maze.js
@@ -1,12 +1,28 @@
 
-window.onload = function(){
-  document.getElementById("boundary1").addEventListener("mouseover", changeClass);
-}
+window.onload = initialize;
 
 
-
+/*
 function changeClass() {
     document.getElementById("boundary1").classList.add("youlose")
 
 };
+*/
 
+function changeClass2() {
+  var x = document.getElementsByClassName("boundary");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].classList.add("youlose");
+  }
+};
+
+function initialize(){
+  //document.getElementById("boundary1").addEventListener("mouseover", changeClass);
+
+  var x = document.querySelectorAll(".boundary");
+  var i;
+  for (i = 0; i < x.length; i++) {
+      x[i].addEventListener("mouseover", changeClass2);
+  }
+}
