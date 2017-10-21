aa19199ce1a57f870b85caf539cb2132172d690a
RishawnG <32467633+RishawnG@users.noreply.github.com>
2017-10-20 21:09:43 -0500

    Ex 6
    Completed exercise 6

--- maze.js
+++ maze.js
@@ -15,6 +15,7 @@ function changeClass2() {
 function initialize(){
   document.getElementById("end").addEventListener("mouseover",youwin);
   document.getElementById("start").addEventListener("click",changeClass);
+  document.getElementById("start").addEventListener("mouseover",running);
   var x = document.querySelectorAll(".boundary");
   var i;
   for (i = 0; i < x.length; i++) {
@@ -34,10 +35,16 @@ function changeClass() {
   state=true;
   var x = document.querySelectorAll(".boundary");
   console.log("test")
-  document.getElementById("status").innerHTML = "Move your mouse over the ''S'' to begin."; 
+  document.getElementById("status").innerHTML = "Move your mouse to E to win"; 
+  
   var i;
   for (i = 0; i < x.length; i++) {
     x[i].classList.remove("youlose");
   }
 
 };
+
+
+function running(){
+  document.getElementById("maze").addEventListener("mouseleave",changeClass2)
+}
